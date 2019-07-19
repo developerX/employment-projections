f27e95a24b740e1f9428f0741648489855471d6f
developerX <x@developerx.io>
2019-07-18 22:39:46 -0700

    updated to port 80

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 app.get('/', (req, res) => {
+  res.send("I dream of being a website");
+});
+
+app.get('/careers', (req, res) => {
   res.send(careers);
 });
 
-app.listen(3000, () => console.log('Gator app listening on port 3000!'));
+app.listen(80, () => console.log('Gator app listening on port 80!'));
