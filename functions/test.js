edd4822659647642b51dd14645fefa0c8cce30f0
developerX <x@developerx.io>
2019-07-18 23:28:03 -0700

    added git ignore and set up lambda

--- functions/test.js
+++ functions/test.js
@@ -1,8 +1,8 @@
-const careers = require('./career-filtered.json');
+const careers = require('../career-filtered.json');
 
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: careers
-  })
+    body: JSON.stringify(careers)
+  });
 }
